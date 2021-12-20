4ad4719b9be99c8754565b3dcd13a61e3561ab95
Santiago Marulanda <santiagomm1997@gmail.com>
2021-12-20 01:13:49 -0500

    Primitive data types

--- src/main.go
+++ src/main.go
@@ -27,4 +27,34 @@ func main() {
 	// Other way to define variables
 	x := "Other way to define variables is using ':='"
 	fmt.Println(x)
+
+	// Primitive types
+	//
+	// Integers:
+	//
+	// int = Depending of the OS (32 or 64 bits)
+	// int8 = 8bits = -128 a 127
+	// int16 = 16bits = -2^15 to 2^15-1
+	// int32 = 32bits = -2^31 to 2^31-1
+	// int64 = 64bits = -2^63 to 2^63-1
+	//
+	// Positive integers:
+	//
+	// uint8 = 8bits = 0 to 127
+	// uint16 = 16bits = 0 to 2^16-1
+	// uint32 = 32bits = 0 to 2^32-1
+	// uint64 = 64bits = 0 to 2^64-1
+	//
+	// Decimals:
+	//
+	// float32 = 32 bits = +/- 1.18e^-38 to +/- -3.4e^38
+	// float64 = 64 bits = +/- 2.23e^-308 to +/- -1.8e^308
+	//
+	// Text:
+	//
+	// string = "" Only double quotes
+	//
+	// Booleans:
+	//
+	// bool = true or false
 }
